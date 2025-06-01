5aa0f0c3e6b64cd78cc3c5001063fe2b19aeb529
Milan Soucek <milan.soucek.2004@gmail.com>
2025-06-01 10:50:19 +0200

    calculation services

--- src/main/java/com/example/mortgageCalc/servis/calculationService.java
+++ src/main/java/com/example/mortgageCalc/servis/calculationService.java
@@ -1,34 +1,44 @@
 package com.example.mortgageCalc.servis;
 
 import com.example.mortgageCalc.enums.Intervals;
+import com.example.mortgageCalc.model.Calculator;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class calculationService {
 
-    public double regularPayment(double loanAmount, double interestRate, double repaymentPeriod, Intervals paymentInterval) {
+    public static double regularPayment(Calculator calculator) {
 
-        double i = (interestRate/100)/paymentInterval.getFrequency();
-        double n = repaymentPeriod*paymentInterval.getFrequency();
+        double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
+        double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
 
-        return loanAmount * (i/(1 - Math.pow(i+1, -n)));
+        return calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
     };
 
-    public double totalPaid(double loanAmount, double interestRate, double repaymentPeriod, Intervals paymentInterval) {
-        double i = (interestRate/100)/paymentInterval.getFrequency();
-        double n = repaymentPeriod*paymentInterval.getFrequency();
-        double regularPayment = loanAmount * (i/(1 - Math.pow(i+1, -n)));
+    public static double totalPaid(Calculator calculator) {
+        double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
+        double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
+        double regularPayment = calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
 
 
         return regularPayment * n;
     };
-    public double paidInInterest(double loanAmount, double interestRate, double repaymentPeriod, Intervals paymentInterval) {
-        double i = (interestRate/100)/paymentInterval.getFrequency();
-        double n = repaymentPeriod*paymentInterval.getFrequency();
-        double regularPayment = loanAmount * (i/(1 - Math.pow(i+1, -n)));
+    public static double paidInInterest(Calculator calculator) {
+        double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
+        double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
+        double regularPayment = calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
 
         double totalPaid = regularPayment * n;
 
-        return totalPaid - loanAmount;
+        return totalPaid - calculator.getLoanAmount();
+    };
+    public List<Double> allCalculations(Calculator calculator) {
+        double regularPayment = regularPayment(calculator);
+        double totalPaid = totalPaid(calculator);
+        double paidInInterest = paidInInterest(calculator);
+        return Arrays.asList(regularPayment, totalPaid, paidInInterest);
     };
 }
