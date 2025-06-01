e394a2c5ca27c9de566b680a6b9eed1f289e71ae
Milan Soucek <milan.soucek.2004@gmail.com>
2025-06-01 09:25:39 +0200

    initial commit

---
+++ src/main/java/com/example/mortgageCalc/MortgageCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mortgageCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mortgageCalc/MortgageCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mortgageCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
