e9d96b666779cfc514719a6195c32c7a9f68146f
AdamRozba <adamrozba@seznam.cz>
2025-06-01 11:06:14 +0200

    rounding

--- src/main/java/com/example/mortgageCalc/servis/CalculationService.java
+++ src/main/java/com/example/mortgageCalc/servis/CalculationService.java
@@ -1,6 +1,7 @@
 package com.example.mortgageCalc.servis;
 
 import com.example.mortgageCalc.model.Calculator;
+import jdk.jfr.Frequency;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -13,26 +14,28 @@ public class CalculationService {
 
         double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
         double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
-
-        return calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
+        double paymentRegular = calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
+        return Math.round(paymentRegular * 100.0) / 100.0;
     };
 
     public static double totalPaid(Calculator calculator) {
         double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
         double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
         double regularPayment = calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
+        double paidTotal = regularPayment * n ;
 
+        return  Math.round(paidTotal * 100.0) / 100.0;
 
-        return regularPayment * n;
     };
     public static double paidInInterest(Calculator calculator) {
         double i = (calculator.getInterestRate()/100)/calculator.getPaymentInterval().getFrequency();
         double n = calculator.getRepaymentPeriod()*calculator.getPaymentInterval().getFrequency();
         double regularPayment = calculator.getLoanAmount() * (i/(1 - Math.pow(i+1, -n)));
-
         double totalPaid = regularPayment * n;
 
-        return totalPaid - calculator.getLoanAmount();
+        double painInterest =  totalPaid - calculator.getLoanAmount();
+        return  Math.round(painInterest * 100.0) / 100.0;
+
     };
     public List<Double> allCalculations(Calculator calculator) {
         double regularPayment = regularPayment(calculator);
